522ea648cd50880d002e05766ffcfab6f9791267
Glen Johnson <glen.johnson@qsuper.qld.gov.au>
2023-05-16 11:55:16 +1000

    init

---
+++ src/main/java/org/github/gjohn/checks/MethodLimitCheck.java
@@ -0,0 +1,42 @@
+package org.github.gjohn.checks;
+
+import com.puppycrawl.tools.checkstyle.api.*;
+
+public abstract class MethodLimitCheck extends AbstractCheck
+{
+    private static final int DEFAULT_MAX = 5;
+    private int max = DEFAULT_MAX;
+
+    @Override
+    public int[] getDefaultTokens()
+    {
+        return new int[]{TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF};
+    }
+
+    @Override
+    public int[] getAcceptableTokens() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] getRequiredTokens() {
+        return new int[0];
+    }
+
+    @Override
+    public void visitToken(DetailAST ast)
+    {
+        // find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
+        DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+
+        // count the number of direct children of the OBJBLOCK
+        // that are METHOD_DEFS
+        int methodDefs = objBlock.getChildCount(TokenTypes.METHOD_DEF);
+
+        // report violation if limit is reached
+        if (methodDefs > this.max) {
+            String message = "too many methods, only " + this.max + " are allowed";
+            log(ast.getLineNo(), message);
+        }
+    }
+}
