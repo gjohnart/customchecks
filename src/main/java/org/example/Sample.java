2c701fb89613ecbf2ed76d5fd9b5201cd513f535
Glen Johnson <glen.johnson@qsuper.qld.gov.au>
2023-05-17 13:48:43 +1000

    init

---
+++ src/main/java/org/example/Sample.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class Sample {
+
+    public long variable = 123456l;
+
+    public void foo(){
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+        System.out.println("fof");
+
+    }
+}
